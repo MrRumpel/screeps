2390f58dd243088b131e1b6b009c313adc695aeb
Rumpel <hades137747575@gmail.com>
2019-05-24 09:10:57 +0800

    官方教程 第四步

--- src/createCreep/createCreep.ts
+++ src/createCreep/createCreep.ts
@@ -1,23 +1,50 @@
 export class CreateCreep {
   public static harvester(creep: Creep): void {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if(creep.carry.energy < creep.carryCapacity) {
       const sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+      if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
       }
     }
-    else if (Game.spawns["Spawn1"].energy < Game.spawns['Spawn1'].energyCapacity) {
-      if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.spawns['Spawn1']);
+    else {
+      const targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
+            structure.energy < structure.energyCapacity;
+        }
+      });
+      if(targets.length > 0) {
+        if(creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        }
       }
     }
   }
 
   public static createHarvest(spawnName: string, hash: string): void {
-    Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], hash, {memory: {role: 'harvest'}});
+    Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], hash, {memory: {role: 'harvester'}});
   }
 
   public static upgrader(creep: Creep):void {
-    console.log(JSON.stringify(creep));
+    if(creep.memory.upgrading && creep.carry.energy === 0) {
+      creep.memory.upgrading = false;
+      creep.say('🔄 harvest');
+    }
+    if(!creep.memory.upgrading && creep.carry.energy === creep.carryCapacity) {
+      creep.memory.upgrading = true;
+      creep.say('⚡ upgrade');
+    }
+
+    if(creep.memory.upgrading) {
+      if(creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+      }
+    }
+    else {
+      const sources = creep.room.find(FIND_SOURCES);
+      if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -24,12 +24,28 @@ export const loop = ErrorMapper.wrapLoop(() => {
       CreateCreep.upgrader(creep);
     }
   }
-  for (const spawnName in Game.spawns) {
-    const hash = new Date().getTime().toString();
-    if (countCreeps < 3) {
-      CreateCreep.createHarvest(spawnName, hash);
-    }
-  }
+  // for (const spawnName in Game.spawns) {
+  //   const hash = new Date().getTime().toString();
+  //   if (countCreeps < 3) {
+  //     CreateCreep.createHarvest(spawnName, hash);
+  //   }
+  // }
   const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
   console.log('Harvesters: ' + harvesters.length);
+
+  if(harvesters.length < 2) {
+    const newName = 'Harvester' + Game.time;
+    console.log('Spawning new harvester: ' + newName);
+    Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+      {memory: {role: 'harvester'}});
+  }
+  if(Game.spawns['Spawn1'].spawning) {
+    const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+    Game.spawns['Spawn1'].room.visual.text(
+      '🛠️' + spawningCreep.memory.role,
+      Game.spawns['Spawn1'].pos.x + 1,
+      Game.spawns['Spawn1'].pos.y,
+      {align: 'left', opacity: 0.8});
+  }
+
 });
