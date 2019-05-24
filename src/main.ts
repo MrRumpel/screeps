8ede16f95861653a2ebc69342c58488d0ccab5fc
Rumpel <hades137747575@gmail.com>
2019-05-24 09:18:26 +0800

    官方教程 第四步 修正一些语法问题，暂时不优雅

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ export const loop = ErrorMapper.wrapLoop(() => {
       {memory: {role: 'harvester'}});
   }
   if(Game.spawns['Spawn1'].spawning) {
-    const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+    const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning!.name];
     Game.spawns['Spawn1'].room.visual.text(
       '🛠️' + spawningCreep.memory.role,
       Game.spawns['Spawn1'].pos.x + 1,
